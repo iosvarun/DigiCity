ce3f25fd1e9f93111d2e060dc64214ddce50abab
varunios <ios.varun@gmail.com>
2016-08-24 12:06:24 +0530

    Initial Commit

---
+++ DigiCity/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DigiCity
+//
+//  Created by Varun Kumar on 24/08/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
