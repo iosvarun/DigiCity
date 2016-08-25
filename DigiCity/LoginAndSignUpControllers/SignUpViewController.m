92f1d66a6199fe85be856f0c93ff225e1a38e777
nav-k <nkothiyal11@gmail.com>
2016-08-25 14:47:22 +0530

    added IBOutlet for signup controller

--- DigiCity/LoginAndSignUpControllers/SignUpViewController.m
+++ DigiCity/LoginAndSignUpControllers/SignUpViewController.m
@@ -7,7 +7,56 @@
 //
 
 #import "SignUpViewController.h"
+#import "AppDelegate.h"
+#import "Constants.h"
+@interface SignUpViewController(){
+    
+    __weak IBOutlet UIScrollView *scrollView;
+    __weak IBOutlet UIView *contentView;
+    
+    __weak IBOutlet UIView *containerView_textFields;
+    __weak IBOutlet UITextField *textField_userName;
+    __weak IBOutlet UITextField *textField_mobileNo;
+    __weak IBOutlet UITextField *textField_password;
+    __weak IBOutlet UITextField *textField_email;
+    float keyboardHeight;
+
+}
+
+
+@end
 
 @implementation SignUpViewController
 
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    [self setKeyBoardObserver];
+}
+
+#pragma mark - Keyboard notification Center
+-(void)setKeyBoardObserver{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+-(void)keyboardWillShow:(NSNotification *)notification{
+    NSDictionary* keyboardInfo = [notification userInfo];
+    NSValue* keyboardFrameBegin = [keyboardInfo valueForKey:UIKeyboardFrameBeginUserInfoKey];
+    CGRect keyboardFrameBeginRect = [keyboardFrameBegin CGRectValue];
+    keyboardHeight = keyboardFrameBeginRect.size.height;
+    NSLog(@"%f",keyboardFrameBeginRect.size.height);
+
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
+    [scrollView setContentOffset:CGPointMake(0, 0)];
+}
+
+#pragma mark - UIButton Action
+- (IBAction)btnActionRegister:(id)sender {
+    [AppDelegateCons() setMainTabbarControllerAsWindowRoot];
+}
 @end
