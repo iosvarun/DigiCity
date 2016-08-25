7b027587495e17baafd952befffc4789044a118c
nav-k <nkothiyal11@gmail.com>
2016-08-25 10:56:45 +0530

    added a new VC in storyboard and a class name : ForgotPasswordViewController.

---
+++ DigiCity/LoginAndSignUpControllers/ForgotPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgotPasswordViewController.h
+//  DigiCity
+//
+//  Created by Naveen on 25/08/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgotPasswordViewController : UIViewController
+
+@end
